a95cd96495537fcaafe2cb6c4548ae5811b52f1d
itcast <x123450000@qq.com>
2026-04-25 17:01:57 +0800

    新增菜品业务代码开发

--- sky-common/src/main/java/com/sky/utils/AliOssUtil.java
+++ sky-common/src/main/java/com/sky/utils/AliOssUtil.java
@@ -32,7 +32,7 @@ public class AliOssUtil {
         OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
 
         try {
-            // 创建PutObject请求。
+            // 创建PutObject请求
             ossClient.putObject(bucketName, objectName, new ByteArrayInputStream(bytes));
         } catch (OSSException oe) {
             System.out.println("Caught an OSSException, which means your request made it to OSS, "
