b262c3a39e972eab8c369f3e1d034cf92e169852
itcast <x123450000@qq.com>
2026-05-06 22:45:05 +0800

    订单数据统计功能代码开发

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -2,6 +2,7 @@ package com.sky.controller.admin;
 
 import com.sky.result.Result;
 import com.sky.service.ReportService;
+import com.sky.vo.OrderReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 import io.swagger.annotations.Api;
@@ -56,6 +57,15 @@ public class ReportController {
         return Result.success(userReportVO);
     }
 
-
+    //订单统计
+    @GetMapping("/ordersStatistics")
+    @ApiOperation("订单统计")
+    public Result<OrderReportVO> ordersStatistics(
+            @DateTimeFormat(pattern="yyyy-MM-dd") LocalDate begin,
+            @DateTimeFormat(pattern="yyyy-MM-dd") LocalDate end){
+        log.info("订单数据统计：{},{}",begin,end);
+        OrderReportVO orderReportVO = reportService.getOrderStatistics(begin,end);
+        return Result.success(orderReportVO);
+    }
 
 }
--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -71,4 +71,7 @@ public interface OrderMapper {
      */
     Double sumByMap(Map map);
 
+    //根据动态条件统计订单数量
+    Integer countByMap(Map map);
+
 }
