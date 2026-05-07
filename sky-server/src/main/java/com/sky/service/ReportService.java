bcec61d5a0038dda8d51dd5eb7450a5b73d41af1
itcast <x123450000@qq.com>
2026-05-07 09:22:52 +0800

    导出运营数据报表

--- sky-server/src/main/java/com/sky/service/ReportService.java
+++ sky-server/src/main/java/com/sky/service/ReportService.java
@@ -5,6 +5,7 @@ import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 public interface ReportService {
@@ -28,5 +29,8 @@ public interface ReportService {
 
     //统计指定时间区间内的销量排名前10
     SalesTop10ReportVO getSalesTop10(LocalDate begin, LocalDate end);
+
+    // 导出运营数据报表
+    void exportBusinessData(HttpServletResponse response);
 
 }
